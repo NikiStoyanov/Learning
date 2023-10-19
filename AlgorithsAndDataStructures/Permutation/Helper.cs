e48ee84b03a3d14baad10995f44777613dfc6898
Nikolay Stoyanov <nikistoyanov2005@gmail.com>
2023-10-19 11:22:40 +0300

    Algorithms and Data Structures

---
+++ AlgorithsAndDataStructures/Permutation/Helper.cs
@@ -0,0 +1,18 @@
+﻿namespace Permutation;
+
+internal class Helper
+{
+    /// <summary>
+    /// Swap elements in an array
+    /// </summary>
+    /// <typeparam name="T">Type of the structure</typeparam>
+    /// <param name="array">Data Structure</param>
+    /// <param name="first">First element</param>
+    /// <param name="second">Second element</param>
+    public static void Swap<T>(T[] array, int first, int second)
+    {
+        var temp = array[first]; 
+        array[first] = array[second]; 
+        array[second] = temp;
+    }
+}
